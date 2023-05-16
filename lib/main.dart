7c466b1b812a0ceaad8689cf4f7523fe327d0911
alldone03 <aldanarsenal@gmail.com>
2023-05-16 20:29:38 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expensetracker/expenses.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    ));
