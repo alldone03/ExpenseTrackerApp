9c5e537cb3eb41387334e2a78992690f6104cdfe
alldone03 <aldanarsenal@gmail.com>
2023-05-20 02:35:29 +0700

    creating Dummy Data

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -18,7 +18,7 @@ class Expense {
   }) : id = uuid.v4();
   final String id;
   final String title;
-  final String amount;
+  final double amount;
   final DateTime date;
   final Category category;
 }
